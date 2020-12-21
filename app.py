da7d622894c077d4da7e3d783b3817e6736cef18
tmarton1 <tanya.marton@gmail.com>
2020-12-20 16:16:36 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,34 +5,49 @@ import pandas as pd
 from bokeh.plotting import figure
 from bokeh.embed import components
 
-
+def get_date_string(first_date,delta_days):
+    date_format='%Y-%m-%d'
+    date=first_date+datetime.timedelta(days=delta_days)
+    if int(date.strftime('%w')) in range(1,6):
+        return date.strftime(date_format)
+    else:
+        pass
+    
 # function to get data
 def getData(ticker):
-    # requesting data from Quandl
-    start = "2020-11-01"
-    end = "2020-11-30"
-    reqUrl = 'https://www.quandl.com/api/v3/datasets/WIKI/' + ticker + '.json?start_date=' + start \
-              + '&end_date=' + end + '&api_key=ky4zqe5WFwms5Tx2NXGx'
-    r = requests.get(reqUrl)
+    reqUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol='+ticker+'&apikey=ML4QI9IK9E7OVNPV'
+    r=requests.get(reqUrl)
+    return_data=r.json()
 
-    # fetch data
-    return_data = r.json()['dataset']
-    df = pd.DataFrame(return_data['data'], columns=return_data['column_names'])
-    df.columns = [col.lower() for col in df.columns]
-    df = df.set_index(pd.DatetimeIndex(df['date']))
+    date_format='%Y-%m-%d'
+    last_date=datetime.datetime.strptime('2020-10-31',date_format)
+    first_date=datetime.datetime.strptime('2020-09-30',date_format)
+    max_delta_days=(last_date-first_date).total_seconds()/(24*60*60)
+
+    date_strings=[(delta_days,get_date_string(first_date,delta_days)) for delta_days in range(1,int(max_delta_days)+1)]
+
+    prices=[]
+    dates=[]
+    days=[]
+    for ds in date_strings:
+        try:
+            price=float(return_data['Time Series (Daily)'][ds[1]]['4. close'])
+            prices.append(price)
+            dates.append(ds[1])
+            days.append(ds[0])
+        except:
+            pass
+    df=pd.DataFrame(list(zip(dates,days,prices)), columns=['date','day','closing price'])
     return df
 
 
 # function to get plot
-def getPlot(df, priceTypes, ticker):
-    p = figure(title="Quandl End of Day Stock prices 2020, Month of November", x_axis_type="datetime", x_axis_label="Date",
-               y_axis_label="Stock price", plot_width=1000)
+def getPlot(df, ticker):
+    p = figure(title="Alphavantage Closing Stock prices 2020, Month of October", x_axis_label="Day in October",
+               y_axis_label="Stock price", x_axis_type='datetime', plot_width=400)
 
-    mapping = {'open': 'open', 'adjOpen': 'adj. open', 'close': 'close', 'adjClose': 'adj. close'}
-    colour = {'open': 'orange', 'adjOpen': 'red', 'close': 'blue', 'adjClose': 'green'}
-
-    for priceType in priceTypes:
-        p.line(df.index, df[mapping[priceType]], color=colour[priceType], legend=ticker + ": " + mapping[priceType])
+    p.annulus(x=df['day'], y=df['closing price'], color='red', legend_label=ticker,inner_radius=0.2, outer_radius=0.25)
+    p.line(x=df['day'], y=df['closing price'], color='red')
     return p
 
 
@@ -54,10 +69,9 @@ def graph():
     # User inputs from the index.html
     ticker = request.form['ticker']
     ticker = ticker.upper()
-    priceTypes = request.form.getlist('priceType')
 
     data = getData(ticker)
-    plot = getPlot(data, priceTypes, ticker)
+    plot = getPlot(data, ticker)
 
     script, div = components(plot)
     reqUrl = "https://www.google.com/finance?q=" + ticker
